4b8cedc23a95174d4eed7360c1a7b8eeed676b11
Hamman Ibrahim Khalil <ibrahimhamman215@gmail.com>
2023-08-15 13:12:34 -0700

    Update Project.tsx

--- components/Project.tsx
+++ components/Project.tsx
@@ -54,11 +54,6 @@ function Project({ project }: Props) {
         </div> */}
         <div className="space-x-3">
           <button className="bg-transparent border border-gray-400 p-1 rounded font-semibold text-gray-400 hover:bg-gray-400 hover:text-gray-900 duration-300 hover:animate-pulse tracking-wider">
-            <Link href={project.gitUrl} target="_blank">
-              GitHub
-            </Link>
-          </button>
-          <button className="bg-transparent border border-gray-400 p-1 rounded font-semibold text-gray-400 hover:bg-gray-400 hover:text-gray-900 duration-300 hover:animate-pulse tracking-wider">
             <Link href={project.liveUrl} target="_blank" >
               Preview
             </Link>
