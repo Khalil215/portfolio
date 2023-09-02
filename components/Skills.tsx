c2351231271b9567b6e0672c2ba22701d6ebbe6a
Hamman Ibrahim Khalil <ibrahimhamman215@gmail.com>
2023-09-02 14:35:59 -0700

    Update Skills.tsx

--- components/Skills.tsx
+++ components/Skills.tsx
@@ -24,7 +24,7 @@ function Skills({ skill }: Props) {
                 whileInView={{ x: 0, opacity: 1 }}
                 transition={{ duration: 1.5 }}
                 viewport={{ once: true }}
-                className="relative w-20 h-20"
+                className="relative md:w-20 w-16 h-16 md:h-20"
               >
                 <Image
                   key={skill.title}
@@ -47,7 +47,7 @@ function Skills({ skill }: Props) {
                 whileInView={{ x: 0, opacity: 1 }}
                 transition={{ duration: 1.5 }}
                 viewport={{ once: true }}
-                className="relative w-20 h-20"
+                className="relative md:w-20 w-16 h-16 md:h-20"
               >
                 <Image
                   key={skill.title}
